88c309bbd448819771247a24979413db835a6d10
Tanya <tanlull@gmail.com>
2018-08-26 22:15:28 +0700

    modeify sol + html work!

--- contracts/Lottoreum.sol
+++ contracts/Lottoreum.sol
@@ -9,49 +9,46 @@ contract  Lottoreum {
         uint balance ; //งวดที่
         bool avaiable;
     }
-    
-    Lotto[]  lottos;   
+
+    Lotto[]  lottos;
     uint nonce = 0;
-    
-    
-    function rand(uint min, uint max) returns (uint){
-        nonce++;
-        return uint(sha3(nonce))%(min+max)-min;
-    }
+
+
+    // function rand(uint min, uint max) returns (uint){
+    //     nonce++;
+    //     return uint(sha3(nonce))%(min+max)-min;
+    // }
 
     constructor() public { // constructure is called once it is deployed to blockchain
         balance = 100; //initial balance
         generateLottos(5);
 
     }
-    
+
     function setBalance(uint newBalance) public {
         balance = newBalance;
     }
-    
+
     function getBalance() public view returns (uint) {
         return balance;
     }
-    
-    
+
+
     function generateLottos(uint maxLotto) public{
         for(uint i=1;i<maxLotto;i++){
-            Lotto lot; 
+            Lotto memory lot;
             lot.id = i;
             lot.gain = i*10;
             lot.balance = i*100;
             lot.avaiable = true;
             lottos.push(lot);
         }
-        
+
     }
-    
-    function getLottobyID(uint i) public returns(uint id, uint gain, uint balance,bool avaiable ){
+
+    function getLottobyID(uint i) public view returns(uint, uint, uint,bool ){
 
            return (lottos[i].id,lottos[i].gain,lottos[i].balance,lottos[i].avaiable);
     }
-    
+
 }
- 
-
-
